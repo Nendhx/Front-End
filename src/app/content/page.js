db84997ddb9ecc749c7a55a9f0ee378cf872440d
12bSiilen <lily.batsaikhan@gmail.com>
2026-05-06 13:54:07 +0800

    okay

--- src/app/content/page.js
+++ src/app/content/page.js
@@ -16,20 +16,21 @@ export default function Home() {
       </div>
       <div className="w-full grid grid-cols-6 gap-4">
         {jsonData.filter((item) => item.first_name.toLowerCase().includes(nameSearch.toLowerCase())).map((item) => (
-          <div className="p-4 border rounded-2xl flex flex-col gap-2" key={item.id}>
-            <div className="flex gap-2 items-center">
-              <img src={item.image} className="w-16"></img>
-              <div className="flex flex-col w-full">
-                <h2 className="font-bold text-lg">{item.first_name}</h2>
-                {(item.type == "student") ? <div className="bg-blue-400 text-blue-600 px-2 py-1 rounded-full font-bold">{item.type}</div> : <div className="bg-green-400 text-green-800 px-2 py-1 rounded-full font-bold">{item.type}</div>}
+          <div className="p-4 border rounded-2xl flex flex-col justify-between gap-2" key={item.id}>
+            <div className="flex flex-col gap-2">
+              <div className="flex gap-2 items-center">
+                <img src={item.image} className="w-16"></img>
+                <div className="flex flex-col w-full">
+                  <h2 className="font-bold text-lg">{item.first_name}</h2>
+                  {(item.type == "student") ? <div className="bg-blue-400 text-blue-600 px-2 py-1 rounded-full font-bold">{item.type}</div> : <div className="bg-green-400 text-green-800 px-2 py-1 rounded-full font-bold">{item.type}</div>}
+                </div>
               </div>
+              <p>Age: {item.age}</p>
+              <p>Email: {item.email}</p>
+              <p>Country: {item.country}</p>
+              <p>Point: {item.point}</p>
+              <p>Created: {item.created_at}</p>
             </div>
-            <p>Age: {item.age}</p>
-            <p>Email: {item.email}</p>
-            <p>Country: {item.country}</p>
-            <p>Point: {item.point}</p>
-            <p>Created: {item.created_at}</p>
-            
             <button className="bg-red-600 hover:bg-red-500 text-white rounded-lg px-4 py-2">Delete</button>
           </div>
         ))}
